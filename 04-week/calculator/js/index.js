1280491293a227179553f9dee2491fd2e5418fc6
katalsman <katalsman@gmail.com>
2016-03-29 19:02:36 -0700

    JS practice

---
+++ 04-week/calculator/js/index.js
@@ -0,0 +1,19 @@
+//alert("Hello FEWD")
+console.log("This is a secret message for devs only")
+console.log(123)
+
+var number = 9
+console.log(number)
+
+var number1 = 152
+console.log(number1)
+
+var number2 = 40
+console.log(number2)
+
+console.log(number1 + number2)
+
+
+
+var number3 = prompt("Enter a number")
+console.log(number3)
